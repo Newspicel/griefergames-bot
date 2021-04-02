d67f48b7539df7f84534bcf32ea12630c39167fc
Julian H <newspicel11@gmail.com>
2021-04-02 13:11:24 +0200

    Add .catch

--- src/tasks/connector.ts
+++ src/tasks/connector.ts
@@ -91,7 +91,7 @@ async function run(bot: Bot, options: ConnectorOptions): Promise<void> {
   listenForSpawn(bot);
 
   try {
-    await bot.client.navigate.promise.to(startPos).then().catch(() => {});
+    await bot.client.navigate.promise.to(startPos).then().catch((error) => {console.log(error)});
   } catch (e) {
     if(bot.options.setPortalTimeout){
       clearTimeout(timeout);
